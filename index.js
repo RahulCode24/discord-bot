73a4eb7a3653b0538fe9ecb782d0867de147ef1e
Rahul Soni <rahul.soni.code@gmail.com>
2024-11-28 22:20:06 +0530

    Generating shortid and stores in db and response it to discord

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const shortid = require("shortid");
+const { connectDB } = require("./connection");
+const Url = require("./models/url");
+
+connectDB("mongodb://127.0.0.1:27017/shorturl").then(() => console.log("DB Connected"));
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', (message) => {
+client.on('messageCreate', async (message) => {
     if(message.author.bot) return;
+
+    if(message.content.startsWith("create")){
+        const userUrl = message.content.split("create ")[1];
+        const shortID = shortid();
+        const result = await Url.create({
+            shortId: shortID,
+            redirectURL: userUrl,
+            visitHistory: []
+        });
+        return message.reply({
+            content: `http://localhost:8001/data/${shortID} this is shortid for ${userUrl}`
+        });
+    }
+
     message.reply({
         content: "Hi from bot!"
     });
